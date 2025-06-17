95c105c1b768f02b15cf20e6e1adbdc218f6a26f
Bassel.Akram <bassel.akram09@gmail.com>
2025-06-17 05:41:49 +0300

    Updating project structure after changes

--- OdooAutomation/project_structure.sh
+++ OdooAutomation/project_structure.sh
@@ -2,18 +2,23 @@ OdooAutomation/
 ├── core/
 │   └── odoo_connector.py
 ├── odoo_models/
-│   └── credit.py            ← Contains the CreditNote class
+│   └── credit.py                       ← Contains the CreditNote class
 ├── excel_io/
 │   ├── __init__.py
-│   └── excel_reader.py      ← Handles loading/parsing Excel files
+│   └── excel_reader.py                 ← Handles loading/parsing Excel files
 ├── test/
-│   ├── test_excel_reader.py ← Validated Excel reading
-│   ├── test_odoo_connector.py ← Authenticated to Odoo
-│   └── test_create_credit_note.py ← We're working on this one now
+│   ├── test_excel_reader.py            ← Validated Excel reading
+│   ├── test_odoo_connector.py          ← Authenticated to Odoo
+│   └── test_create_credit_note.py      ← We're working on this one now
 ├── data/
-│   └── credit_notes.xlsx          ← Your Excel sheet(s) go here
-│   └── credit_notes_invalid_test.xlsx          ← Your Excel sheet(s) go here
+│   └── credit_notes.xlsx               ← Your Excel sheet(s) go here
+│   └── credit_notes_invalid_test.xlsx  ← Your Excel sheet(s) go here
 │   └── credit_notes_test.xlsx          ← Your Excel sheet(s) go here
-├── .env                     ← Stores ODOO_URL, ODOO_DB, etc.
-├── main.py                  ← Entry point (not fully wired yet)
+├── utils/
+│   └── logger.py                       ← Handles logging into the logs folder
+│   └── validators.py                   ← Contains system validators
+│   └── credit_notes_test.xlsx          ← Your Excel sheet(s) go here
+├── .env                                ← Stores ODOO_URL, ODOO_DB, etc.
+├── .gitignore                          ← For git interaction
+├── main.py                             ← Entry point (not fully wired yet)
 └── requirements.txt
